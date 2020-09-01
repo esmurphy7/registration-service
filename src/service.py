c9adccf84316054cedcf4c9c3571e7aa800f6fb3
Evan Murphy <esmurphy7@gmail.com>
2020-08-31 21:59:34 -0700

    register course feed

--- src/service.py
+++ src/service.py
@@ -1,18 +1,37 @@
 import argparse
 import pickle
 import os.path
-from google.cloud import pubsub_v1
+from __future__ import print_function
+from googleapiclient import errors
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
+from google.cloud import pubsub_v1
 
-def register_domain(project_id, topic_name, classroom_service):
-    publisher = pubsub_v1.PublisherClient()
+def register_feed(classroom_service, body):
+    try:
+        registration = classroom_service.registration().create(body=body).execute()
+    except errors.HttpError as error:
+        print('Failed to register feed.')
+        return error
+    return registration
 
-    # Call the Classroom API
-    results = classroom_service.courses().list(pageSize=10).execute()
-    courses = results.get('courses', [])
-    
+def register_course_feed(project_id, topic_name, classroom_service, course_id):
+    body = {
+        "feed": {
+            "feedType": "COURSE_WORK_CHANGES",
+            "courseWorkChangesInfo": {
+                "courseId": f"{course_id}"
+            }
+        },
+        "cloudPubsubTopic": {
+            "topicName": f"projects/{project_id}/topics/{topic_name}"
+        }
+    }
+
+    registration = register_feed(classroom_service=classroom_service, body=body)
+    return registration
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
@@ -24,13 +43,15 @@ if __name__ == "__main__":
     
     subparsers = parser.add_subparsers(dest="command")
 
-    domain_parser = subparsers.add_parser("domain", help=register_domain.__doc__)
-    domain_parser.add_argument("topic_name")
+    course_parser = subparsers.add_parser("course", help=register_course_feed.__doc__)
 
     args = parser.parse_args()
 
-    SCOPES = ['https://www.googleapis.com/auth/classroom.courses.readonly']
-    # SCOPES = ['https://www.googleapis.com/auth/classroom.push-notifications','https://www.googleapis.com/auth/classroom.rosters.readonly']
+    SCOPES = [
+        'https://www.googleapis.com/auth/classroom.push-notifications',
+        'https://www.googleapis.com/auth/cloud-platform',
+        'https://www.googleapis.com/auth/pubsub'
+    ]
     creds = None
 
     # The file token.pickle stores the user's access and refresh tokens, and is
@@ -44,8 +65,7 @@ if __name__ == "__main__":
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
+            flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
         with open('token.pickle', 'wb') as token:
@@ -53,5 +73,5 @@ if __name__ == "__main__":
 
     classroom_service = build('classroom', 'v1', credentials=creds)
 
-    if args.command == "domain":
-        register_domain(args.project_id, args.topic_name, classroom_service)
+    if args.command == "course":
+        register_course_feed(args.project_id, args.topic_name, classroom_service, course_id)
